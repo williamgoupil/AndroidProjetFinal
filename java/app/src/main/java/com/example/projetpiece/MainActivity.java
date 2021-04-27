3474bd14b8512bb0c35d4df5139e46e545aead50
Samuelfournier <sam.fournier1212@gmail.com>
2021-04-27 15:53:56 -0400

    fixed bug on manifest

--- java/app/src/main/java/com/example/projetpiece/MainActivity.java
+++ java/app/src/main/java/com/example/projetpiece/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
