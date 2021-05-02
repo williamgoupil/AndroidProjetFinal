71f7fc0e8070f0bfa13562e32d65713b2cb381d1
OlivierVigno <oliviervigneault10@gmail.com>
2021-05-02 13:47:24 -0400

    merge requests to API

--- java/app/src/main/java/com/example/projetpiece/AdapterEmprunt.java
+++ java/app/src/main/java/com/example/projetpiece/AdapterEmprunt.java
@@ -92,7 +92,7 @@ public class AdapterEmprunt extends RecyclerView.Adapter<AdapterEmprunt.MyViewHo
         try {
             //URL pour cancel une certaine commande
             String urlCancel = "https://d11d840bcd81.ngrok.io/api-mobile-annuleremprunt/" + mRecentlyDeletedItem.getId();
-            response = new webApiRequest().execute(urlCancel).get();
+            response = new webApiGetRequest().execute(urlCancel).get();
 
             JSONParser parse = new JSONParser();
             JSONObject data_obj = (JSONObject) parse.parse(response);
